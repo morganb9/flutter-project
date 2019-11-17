b89e75cc9bcc00bcc2deb3b58331ea329acb0ee9
morgan <blaismorgan44@gmail.com>
2019-11-17 20:28:05 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'All Places',
+      title: 'All Places', //mon appli
       theme: ThemeData(
 
         primarySwatch: Colors.indigo,
